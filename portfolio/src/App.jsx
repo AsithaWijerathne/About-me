81f7e5506cbc8d72ac61b7186291f5ac48d156de
AsithaWijerathne <wijerathneasitha@gmail.com>
2025-05-01 14:17:30 +0530

    latest

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>This is a simple React app.</p>
+      <p>Welcome to my portfolio!</p>
+      <p>Check out my projects and skills.</p>
+      <p>Feel free to reach out for collaboration or inquiries.</p>
+      <p>Thank you for visiting!</p>
+    </div>
+  );
+}
+
+export default App;
