d639ec6e0ac48e22f60b43cd3a538a3a2ecdbbf4
AsithaWijerathne <wijerathneasitha@gmail.com>
2025-05-02 21:35:30 +0530

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <p>This is a simple React app.</p>
-      <p>Welcome to my portfolio!</p>
-      <p>Check out my projects and skills.</p>
-      <p>Feel free to reach out for collaboration or inquiries.</p>
-      <p>Thank you for visiting!</p>
+      <h1 className="name">
+        <span className="fname">Asitha</span>
+        <span className="lname">Wijerathne</span>
+      </h1>
     </div>
   );
 }
